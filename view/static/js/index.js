318a88d688b82384cdbed9c40912944a9816a276
Jarao <j.wroiate@gmail.com>
2020-06-27 01:17:46 +0800

    add fancybox replace modal

--- view/static/js/index.js
+++ view/static/js/index.js
@@ -1,4 +1,3 @@
-$('#myModal').on('shown.bs.modal', function () { $('#myInput').trigger('focus') })
 {
     let h
     $('body').on('show.bs.modal', ".modal", () => {
@@ -11,8 +10,6 @@ $('#myModal').on('shown.bs.modal', function () { $('#myInput').trigger('focus') 
     });
 }
 
-$(function () { $('[data-toggle="tooltip"]').tooltip() })
-
 $("body").on("click", "a#reload", () => {
 
     var ws = new WebSocket("ws://" + window.location.host + "/reload" + $(".tag a.active").attr("href"))
@@ -20,13 +17,26 @@ $("body").on("click", "a#reload", () => {
         let data = JSON.parse(evt.data)
         if (data["total"] != 0) {
             let per = (data["total"] - data["num"]) / data["total"]
-            $("div[role=progressbar]").css("width", per*100 + "%")
-        } 
+            $("div[role=progressbar]").css("width", per * 100 + "%")
+        }
     }
     ws.onclose = function (evt) {
         console.log("Connection closed.")
         $("div[role=progressbar]").css("width", "100%")
-        setTimeout(window.location.reload.bind(window.location),1000) 
+        setTimeout(window.location.reload.bind(window.location), 1000)
     }
 })
 
+$("[data-fancybox]").fancybox({
+    beforeShow: function (instance, slide) {
+        $("a.card img,.pic-info").each(function () {
+            $(this).removeClass("active")
+        })
+        $(slide.opts.$orig).find("img").toggleClass("active");
+        $(slide.opts.$orig).find(".pic-info").toggleClass("active");
+    },
+    afterClose: function (instance, slide) {
+        $(slide.opts.$orig).find("img").removeClass("active");
+        $(slide.opts.$orig).find(".pic-info").removeClass("active");
+    },
+})
